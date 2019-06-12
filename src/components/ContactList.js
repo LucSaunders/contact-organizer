9df20ee4f51d6eb9440022cdad8c2e2ca02aa75a
Luc Saunders <lucsaunders@gmail.com>
2019-06-12 13:08:57 -0400

    Add Context for state management. Complete delete button functionality.

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,44 +1,22 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
+import { Consumer } from '../context';
 
 class ContactList extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'jdoe@email.com',
-        phone: '879-567-5443'
-      },
-      {
-        id: 2,
-        name: 'Karen Hurst',
-        email: 'khurst@email.com',
-        phone: '202-9827-8395'
-      }
-    ]
-  };
-
-  deleteContact = id => {
-    const { contacts } = this.state;
-    // Filter out the contact to be deleted
-    const newContacts = contacts.filter(contact => contact.id !== id);
-    this.setState({ contacts: newContacts });
-  };
-
   render() {
-    const { contacts } = this.state;
-
     return (
-      <React.Fragment>
-        {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            contact={contact}
-            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-          />
-        ))}
-      </React.Fragment>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
