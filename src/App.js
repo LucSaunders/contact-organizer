020687cb3530cac76aebcb2cdd4a94fae1eda625
Luc Saunders <lucsaunders@gmail.com>
2019-06-12 20:26:58 -0400

    Add router and routes.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ContactList from './components/contacts/ContactList';
-import Header from './components/layout/Header';
 import AddContact from './components/contacts/AddContact';
+import Header from './components/layout/Header';
+import About from './components/pages/About';
 import { Provider } from './context';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,13 +13,18 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <div className="App">
-          <Header branding="Contact Organizer" />
-          <div className="container">
-            <AddContact />
-            <ContactList />
+        <Router>
+          <div className="App">
+            <Header branding="Contact Organizer" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={ContactList} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
           </div>
-        </div>
+        </Router>
       </Provider>
     );
   }
