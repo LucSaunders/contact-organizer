f443eb4276f5cb89954e64ccfa4bd8a5e11df94c
Luc Saunders <lucsaunders@gmail.com>
2019-06-12 20:12:01 -0400

    Add error checking: require all fields to be filled out before submitting.

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -7,7 +7,8 @@ class AddContact extends Component {
   state = {
     name: '',
     email: '',
-    phone: ''
+    phone: '',
+    errors: {}
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -16,6 +17,22 @@ class AddContact extends Component {
     e.preventDefault();
 
     const { name, email, phone } = this.state;
+
+    // Check for empty submissions / Require fields be filled out
+    if (name === '') {
+      this.setState({ errors: { name: 'Name is required' } });
+      return;
+    }
+
+    if (email === '') {
+      this.setState({ errors: { email: 'Email is required' } });
+      return;
+    }
+
+    if (phone === '') {
+      this.setState({ errors: { phone: 'Phone is required' } });
+      return;
+    }
 
     const newContact = {
       id: uuid(),
@@ -30,12 +47,13 @@ class AddContact extends Component {
     this.setState({
       name: '',
       email: '',
-      phone: ''
+      phone: '',
+      errors: {}
     });
   };
 
   render() {
-    const { name, email, phone } = this.state;
+    const { name, email, phone, errors } = this.state;
 
     return (
       <Consumer>
@@ -52,6 +70,7 @@ class AddContact extends Component {
                   placeholder="Enter name..."
                   value={name}
                   onChange={this.onChange}
+                  error={errors.name}
                 />
                 <TextInputGroup
                   label="Email"
@@ -60,6 +79,7 @@ class AddContact extends Component {
                   placeholder="Enter email..."
                   value={email}
                   onChange={this.onChange}
+                  error={errors.email}
                 />
                 <TextInputGroup
                   label="Phone"
@@ -67,6 +87,7 @@ class AddContact extends Component {
                   placeholder="Enter phone..."
                   value={phone}
                   onChange={this.onChange}
+                  error={errors.phone}
                 />
 
                 <input
