6a1a588c0ab148801b82f929731e4469dee0e002
Luc Saunders <lucsaunders@gmail.com>
2019-06-20 07:56:41 -0400

    Add more descriptive text to About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,8 +3,12 @@ import React from 'react';
 export default () => {
   return (
     <div>
-      <h1 className="display-4">About Contact Organizer</h1>
-      <p className="lead">Simple app to manage contacts</p>
+      <h1 className="display-4">About</h1>
+      <p className="lead">
+        A basic app to manage contacts, built with React, Context API, Bootstrap
+        4, and Axios. Data is populated using
+        https://jsonplaceholder.typicode.com, for demo purposes.
+      </p>
       <p className="text-secondary">Version 1.0.0</p>
     </div>
   );
