27773b3c07dc3bb791fb704e7550ead463778a65
Luc Saunders <lucsaunders@gmail.com>
2019-06-20 08:07:50 -0400

    Edit text on About page.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,9 +5,13 @@ export default () => {
     <div>
       <h1 className="display-4">About</h1>
       <p className="lead">
-        A basic app to manage contacts, built with React, Context API, Bootstrap
-        4, and Axios. Data is populated using
-        https://jsonplaceholder.typicode.com, for demo purposes.
+        Contact Organizer is a fully responsive app for managing contact
+        information, built with React, Context API, React-Router-DOM, Bootstrap
+        4, and Axios.
+      </p>
+      <p className="lead">
+        Data is populated using https://jsonplaceholder.typicode.com, for demo
+        purposes.
       </p>
       <p className="text-secondary">Version 1.0.0</p>
     </div>
